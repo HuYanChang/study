bac5d2544599a8c5a37f70fcd14e35320dac46cb
hyc <976262957@qq.com>
2018-07-13 18:56:00 +0800

    model

--- app/Model/BaseModel.php
+++ app/Model/BaseModel.php
@@ -5,3 +5,45 @@
  * Date: 2018/7/13
  * Time: 18:42
  */
+
+use Illuminate\Database\Eloquent\Model;
+class BaseModel extends Model{
+    function getNormalSql($querySql, $queryData = []) {
+        $data = DB::select($querySql, $queryData);
+        $data = array_map('get_object_vars', $data);
+        return $data;
+    }
+
+    /**
+     * $fields有值时为post请求
+     * @param string $url
+     * @param  array $fields
+     * @return string
+     */
+    function httpRequest($url, $fields = [])
+    {
+        //使用curl发送post请求
+        $ch = curl_init();
+        //设置请求地址
+        curl_setopt($ch, CURLOPT_URL, $url);
+        //数据返回后不直接显示
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        //禁止证书校验
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        if ($fields) {
+            //设置过期时间
+            curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+            //开启post
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        }
+        $data = '';
+        if (curl_exec($ch)) {
+            //发送成功，获取数据
+            $data = curl_multi_getcontent($ch);
+        }
+        curl_close($ch);
+        return $data;
+    }
+}
--- app/Model/Index.php
+++ app/Model/Index.php
@@ -5,3 +5,8 @@
  * Date: 2018/7/13
  * Time: 18:31
  */
+use Illuminate\Database\Eloquent\Model;
+class Index extends Model{
+    protected $appId = 'wxe35be9a4ca8325f9';
+    protected $appSecret = '181135a5f429f015bba90e5072e81f55';
+}
