bdf84c4802d5206f7495d2caa7ef3a5677d38ebb
HuYanChang <32992565+HuYanChang@users.noreply.github.com>
2019-02-13 11:06:46 +0800

    migrate

---
+++ database/migrations/2019_02_13_030128_create_test_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->comment('用户id');
+            $table->text('content')->comment('内容');
+            $table->integer('create_at')->comment('创建时间');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('test');
+    }
+}
